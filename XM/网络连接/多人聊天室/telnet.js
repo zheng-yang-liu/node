e8fe5bc04be19ea200cf96baf45a3a01287a4bc1
thexu <thexuyan@outlook.com>
2023-04-19 13:49:21 +0800

    多人聊天室

--- XM/网络连接/多人聊天室/telnet.js
+++ XM/网络连接/多人聊天室/telnet.js
@@ -1,12 +1,13 @@
 var net = require('net');
 var client =net.createConnection({
-    host:'127.0.0.1',
+    host:'192.168.43.20',
     port:3000
 });
 client.on('connect',() => {
     console.log('客户端与服务器连接成功');
     process.stdin.on('data',(data)=>{
-        client.write(data.trim());
+        data=data.toString().trim();
+        client.write(data);
     })
 });
 client.on('data',(data) => {
--- XM/网络连接/多人聊天室/user.js
+++ XM/网络连接/多人聊天室/user.js
@@ -2,20 +2,24 @@
 var net =require("net");
 //创建服务器
 var server = net.createServer();
-
+var users=[];
 // 连接上来之后触发connection事件
 server.on('connection', function(socket){
     // 监听socket中的data数据时触发此事件,来获取接收到的数据
+    users.push(socket);
+    console.log('客户端'+socket.remoteAddress+'连接上了');
     socket.on('data', function(data){
-        // 显示ip
-        console.log("ip:"+socket.remoteAddress);
-        // 显示端口号
-        console.log("port:"+socket.remotePort);
-        console.log(data.toString());
-        // 向客户端发送数据
-        // const eat = Buffer.from('我吃的小豆包', 'utf-8');
-        socket.write('我吃的小豆包');
+        data =data.toString().trim();
+        // forEach便利数组
+        users.forEach((client)=>{
+            if(client != socket){
+                client.write(client.remoteAddress+':'+data);
+            }
+        });
     });
+    socket.on('error', function(){
+        console.log('客户端异常退出');
+    })
 });
 
 // 创建listen监听
